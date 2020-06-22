4c39c6200fe18a77acd5e6b43f1549f8aec7d3d2
Othman Andour <andour.otman@gmail.com>
2020-06-22 16:24:37 +0200

    Creating first templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Otman Andour',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date': 'June 22, 2020'
+
+    },
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 2 ',
+        'content': 'Second post content',
+        'date': 'June 23, 2020'
+
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
