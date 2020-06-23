65a38e9f5b7697af255639f4719fa52bf5875cdf
Othman Andour <andour.otman@gmail.com>
2020-06-23 11:32:46 +0200

    Creating databases

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Otman Andour',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date': 'June 22, 2020'
-
-    },
-    {
-        'author': 'John Smith',
-        'title': 'Blog Post 2 ',
-        'content': 'Second post content',
-        'date': 'June 23, 2020'
-
-    }
-]
-
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
