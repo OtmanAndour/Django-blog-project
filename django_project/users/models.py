8bceea212487b01de6f3bb99fb2860d8a5a733ed
Othman Andour <andour.otman@gmail.com>
2020-06-27 13:07:47 +0200

    Edited blog models.py

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
+   """  def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
@@ -17,5 +17,5 @@ class Profile(models.Model):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save()
+            img.save() """
 
